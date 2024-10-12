ac5a78edc8402b8dee581bd7c6535c5499eae1e7
nyenyel <154278884+nyenyel@users.noreply.github.com>
2024-10-12 09:04:07 +0800

    member outlet

--- frontend/src/outlet/admin/MemberOutlet.jsx
+++ frontend/src/outlet/admin/MemberOutlet.jsx
@@ -34,7 +34,6 @@ export default function MemberOutlet() {
           } else {
             console.error('Error Message:', error.message);
           } 
-
     } finally {
       setLoading(false)
       console.log(invCodeRes)
